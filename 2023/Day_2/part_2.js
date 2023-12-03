f0c635447e8267ae3c01fed0f8dc54422ec03489
connormartin12 <connorm7832@gmail.com>
2023-12-02 21:12:15 -0600

    Day 2 part 2 complete

--- 2023/Day_2/part_2.js
+++ 2023/Day_2/part_2.js
@@ -1,34 +1,34 @@
 const fs = require('fs');
 
-const bagContents = {
-    red: 12,
-    green: 13,
-    blue: 14,
-};
-
 fs.readFile('puzzle_input.txt', 'utf8', (error, result) => {
     if (error) {
         console.error(error);
         return;
     }
-
+    
     let sum = 0;
-
+    
     const lines = result.split('\n');
-    lines.forEach((line, gameID) => {
+    lines.forEach(line => {
         let answers = line.split(': ')[1].split('; ');
-        let validAnswer = true;
+        console.log(answers);
+        let bagContents = {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
         answers.forEach(answer => {
             pulls = answer.split(', ');
             pulls.forEach(pull => {
                 number_color = pull.split(' ');
                 let number = parseInt(number_color);
                 let color = number_color[1];
-                if (number > bagContents[color]) validAnswer = false;
+                if (number > bagContents[color]) 
+                    bagContents[color] = number;
             });
         });
 
-        if (validAnswer) sum += gameID + 1;
+        sum += bagContents['red'] * bagContents['green'] * bagContents['blue'];
     });
 
     console.log(`\nSum: ${sum}`);

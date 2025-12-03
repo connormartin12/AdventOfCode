5d2c3bf198212534ef8ce6e59651e52f9984f528
connormartin12 <connorm7832@gmail.com>
2025-12-02 18:36:07 -0600

    Successful part 1

--- 2025/day-1/part-1.ts
+++ 2025/day-1/part-1.ts
@@ -1,1 +1,39 @@
-console.log("hello world");
+import { readFileSync } from "node:fs";
+
+const input: string = readFileSync("2025/day-1/puzzle-input.txt", "utf8");
+const lines: string[] = input.split("\n");
+
+const updateDialPosition = (position: number, rotation: string): number => {
+  const rotationDirection = rotation.charAt(0);
+  const rotationValue = parseInt(rotation.slice(1));
+
+  if (rotationDirection === "R") {
+    position += rotationValue;
+    if (position > 99) {
+      position = updateDialPosition(-1, `${rotationDirection}${position - 99}`);
+    }
+  } else {
+    if (position - rotationValue < 0) {
+      position = updateDialPosition(
+        99,
+        `${rotationDirection}${Math.abs(position - rotationValue + 1)}`
+      );
+    } else {
+      position -= rotationValue;
+    }
+  }
+
+  return position;
+};
+
+let password: number = 0;
+let dialPosition: number = 50;
+for (let i = 0; i < lines.length; i++) {
+  const rotation = lines[i];
+  dialPosition = updateDialPosition(dialPosition, rotation);
+  if (dialPosition === 0) {
+    password += 1;
+  }
+}
+
+console.log("Password -", password);
